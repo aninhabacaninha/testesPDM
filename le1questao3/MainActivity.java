8ea7fdb7928e4e8e4f4c592a772bb9a7e1be16e6
aninhabacaninha <113071941+aninhabacaninha@users.noreply.github.com>
2023-05-02 09:07:09 -0400

    Update MainActivity.java

--- le1questao3/MainActivity.java
+++ le1questao3/MainActivity.java
@@ -12,30 +12,39 @@ import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText editTextAltura;
+    private Spinner spinnerGenero;
+    private Button buttonCalcular;
+    private TextView textViewResultado;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EditText editTextAltura = findViewById(R.id.editText_altura);
-        Spinner spinnerGenero = findViewById(R.id.spinner_genero);
-        Button buttonCalcular = findViewById(R.id.button_calcular);
-        TextView textViewResultado = findViewById(R.id.textView_resultadoPesoIdeal);
+        editTextAltura = findViewById(R.id.editText_altura);
+        spinnerGenero = findViewById(R.id.spinner_genero);
+        buttonCalcular = findViewById(R.id.button_calcular);
+        textViewResultado = findViewById(R.id.textView_resultadoPesoIdeal);
 
         buttonCalcular.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                double h = Double.parseDouble(editTextAltura.getText().toString());
-                double resultadoPesoIdeal = 0;
-                if(spinnerGenero.getSelectedItemPosition() == 0) {
-                    resultadoPesoIdeal = (72.7 * h) - 58;
-                } else {
-                    resultadoPesoIdeal = (62.1 * h) - 44.7;
-                }
-                DecimalFormat formato = new DecimalFormat("#0.00");
-                String resultadoFormatado = "Peso ideal: " + formato.format(resultadoPesoIdeal);
-                textViewResultado.setText(resultadoFormatado);
+                calcularPesoIdeal(editTextAltura, spinnerGenero, textViewResultado);
             }
         });
+    }
+
+    private void calcularPesoIdeal(EditText editTextAltura, Spinner spinnerGenero, TextView textViewResultado) {
+        double h = Double.parseDouble(editTextAltura.getText().toString());
+        double resultadoPesoIdeal = 0;
+        if(spinnerGenero.getSelectedItemPosition() == 0) {
+            resultadoPesoIdeal = (72.7 * h) - 58;
+        } else {
+            resultadoPesoIdeal = (62.1 * h) - 44.7;
+        }
+        DecimalFormat formato = new DecimalFormat("#0.00");
+        String resultadoFormatado = "Peso ideal: " + formato.format(resultadoPesoIdeal);
+        textViewResultado.setText(resultadoFormatado);
     }
 }
